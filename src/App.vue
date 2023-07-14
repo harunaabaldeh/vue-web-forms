d907f5c1e6d30fc3380c62fdeba4025a508bd8a9
harunaabaldeh <harunaabaldeh@gmail.com>
2023-07-14 17:08:47 +0000

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script>
+import SignupForm from "./components/SignupForm.vue";
+export default {
+  name: "app",
+  components: { SignupForm },
+};
+</script>
+
+<template>
+  <h1>Web Forms</h1>
+  <SignupForm />
+</template>
+
+<style>
+#app {
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
+
+body {
+  margin: 0;
+  background: #eee;
+}
+</style>
